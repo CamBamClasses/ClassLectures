c136a3aaca5f36c012cdd7f20474bb31d602d0ca
Cameron Frandsen <cameron.frandsen1@gmail.com>
2024-01-30 11:42:22 -0700

    Demo on vectors

--- vectors.cpp
+++ vectors.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+
+struct Node
+{
+  std::string key;
+  int value;
+};
 
 int main()
 {
-
   std::vector<int> numbers(10, 1);
   std::vector<int> numbers2(10, 1);
+  const auto myNumber = 3;
+  auto secondNumber = myNumber;
+  auto numbers4 = numbers;
 
   for (size_t i = 0; i < numbers.size(); i++)
   {
@@ -13,6 +22,16 @@ int main()
     numbers.at(i) = i;
     // std::cout << numbers[i] << " ";
     // std::cout << numbers.at(i) << std::endl;
+  }
+
+  for (auto itr = numbers.begin(); itr != numbers.end(); itr++)
+  {
+    std::cout << *itr << std::endl;
+  }
+
+  for (auto&& itr : numbers)
+  {
+    std::cout << itr << std::endl;
   }
 
   numbers.push_back(10);
@@ -28,7 +47,22 @@ int main()
   {
     std::cout << "the same!" << std::endl;
   }
+
   std::vector<int> numbers3 = numbers;
+
+  std::vector<Node> nodes;
+
+  nodes.emplace_back("test", 4);
+
+  for (auto&& itr : nodes)
+  {
+    std::cout << itr.key << " " << itr.value << std::endl;
+  }
+
+  std::vector<std::vector<int>> dVector(4, std::vector<int>(4, 3));
+  dVector[0][0];
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
   return 0;
 }
